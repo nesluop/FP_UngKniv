cf0ff265b40159db124ad2a1336834b005c31a4d
pesa <pesa@itu.dk>
2023-02-09 11:01:42 +0100

    templates

---
+++ exercise3.fs
@@ -0,0 +1,41 @@
+module a2
+// Exercise 3.1 downTo + downTo2
+let rec downTo n = failwith "not implemented"
+let rec downTo2 = failwith "not implemented"
+// Exercise 3.2 removeOddIdx
+let rec removeOddIdx (xs: int list) = failwith "not implemented"
+
+// Exercise 3.3 combinePair
+let rec combinePair (xs :int list) = failwith "not implemented"
+
+// Exercise 3.4 - HR 3.2 - British currency
+
+// Money tuple addition
+let (^+^) a b = failwith "not implemented"
+// Money tuple subtraction
+let (^-^) a b = failwith "not implemented"
+
+type Money = {pound : int; shilling : int; pence : int};;
+// Money record addition
+let (|+|) a b = failwith "not implemented"
+// Money record subtraction
+let (|-|) a b = failwith "not implemented"
+
+// Exercise 3.5 - HR 3.3 - Complex numbers
+
+//      1. Declare infix for addition and multiplication
+let ( .+) (a:float,b:float) (c:float,d:float) = failwith "not implemented"
+
+let ( .*) (a:float,b:float) (c:float,d:float) = failwith "not implemented"
+
+//      2. Declare infix for subtraction and division
+let ( .-) (a:float,b:float) (c:float,d:float) = failwith "not implemented"
+
+let ( ./) (a,b) (c,d) = failwith "not implemented"
+//      3. Use 'let' expressions in division to avoid repeated evals
+let ( ../) (a:float,b:float) (c:float,d:float) = failwith "not implemented"
+
+
+// Exercise 3.6 - HR 4.4 - altSum -> HR page 76
+// function alternating between adding and subtracting the contents of a list.
+let rec altsum = failwith "not implemented"
---
+++ exercise4.fs
@@ -0,0 +1,33 @@
+module a2
+// Exercise 4.1 - explode - string to char list
+let explode (s:string) = failwith "not implemented"
+let rec explode2 (s:string) = failwith "not implemented"
+// Exercise 4.2 - implode - char list to string
+let implode list = failwith "not implemented"
+
+let implodeRev list = failwith "not implemented"
+
+// Exercise 4.3 - toUpper
+let toUpper (s:string) = failwith "not implemented"
+
+// explode >> map >> implode
+let toUpper1 (s:string) = failwith "not implemented"
+// let toUpper2 (s:string) = implode (explode s |> List.map(fun x -> (Char.ToUpper(x))));;
+let toUpper2 (s:string) = failwith "not implemented"
+
+// Exercise 4.4 - palindrome - treating empty strings as palindromes too.
+let rec palindrome (s:string) = failwith "not implemented"
+// Exercise 4.5 - ack
+let rec ack t = failwith "not implemented"
+    // ack(3, 11) = 16381
+
+// Exercise 4.6 - time
+let time f = failwith "not implemented"
+    
+let timeArg1 f a = failwith "not implemented"
+
+// Exercise 4.7 - HR 5.4 - downTo2 f n e
+let rec downto1 f (n, e) = failwith "not implemented"
+// factorial function using downto1 for recursion.
+let fact n = failwith "not implemented"
+let buildList g n = failwith "not implemented"
