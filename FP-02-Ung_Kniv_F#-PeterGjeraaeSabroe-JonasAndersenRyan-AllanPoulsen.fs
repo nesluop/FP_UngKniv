1cd2a9e67cd45eeb9dda8224f0bb2f298a4835f8
jrya <jrya@itu.dk>
2023-02-09 10:37:28 +0100

    solve 2.3

--- FP-02-Ung_Kniv_F#-PeterGjeraaeSabroe-JonasAndersenRyan-AllanPoulsen.fs
+++ FP-02-Ung_Kniv_F#-PeterGjeraaeSabroe-JonasAndersenRyan-AllanPoulsen.fs
@@ -5,10 +5,13 @@ let timediff (h0, m0) (h1, m1) =
     (h1*60 + m1)-(h0*60 + m0) //virker ikke hvis den går over midnat, lol
 
 // 2.2 function minutes
-let minutes (h, m) = failwith "not implemented"
+let minutes = timediff(0,0) // timediff ved den skal have 2 arguments, så dem behøver vi ikek skrive
+    // this gcould just as well have been let minutes (h,m) = h*60+m
 
-// 2.3 / HR 2.2
-let rec pow (s,n) = failwith "not implemented"
+// 2.3 / HR 2.2  - we are using some type inference
+let rec pow =
+    | ((text), 0) -> ""
+    | ((text), n) -> text + dupn(text, n-1)
 
 // 2.4 / HR 2.8
 let rec bin (a,b) = failwith "not implemented"
